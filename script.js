865fea8e46616ee1bba2c749240dd71f2315b238
Wallis <wallis.sl@outlook.com>
2025-10-10 14:30:27 -0300

    Animações e localStorelocalStorage

--- script.js
+++ script.js
@@ -13,9 +13,9 @@ form.addEventListener("submit", (e) => {
     let idade = parseInt(document.getElementById("idade").value);
     
     // validação
-    if (nome === "" | isNaN(idade) || idade <=0) {
+    if (nome === "" || isNaN(idade) || idade <=0) {
         alert("Preencha corretamente os campos!");
-        return;
+        return; 
     }
 
     let alunoExiste = alunos.some(aluno => aluno.nome === nome);
@@ -25,13 +25,13 @@ form.addEventListener("submit", (e) => {
         return alert("Já existe um cadastro com esse nome!");
     }
 
-    // criar objeto aluno
-    let aluno = { nome, idade };
+    // criar objeto aluno (com Id único)
+    let aluno = { id: Date.now().toString(), nome, idade }; // Criando com Id único para que ele não se perca na hora de apagar os itens no localStore.
     alunos.push(aluno);
     localStorage.setItem("alunos", JSON.stringify(alunos));
 
     // adiciona só o novo aluno com animação
-    adicionarAlunoNaLista(aluno, alunos.length -1);
+    adicionarAlunoNaLista(aluno);
     somaEMedia()
     // limpar formulário
     form.reset();
@@ -43,6 +43,7 @@ function renderAlunos() {
     if(!alunos.length){
     let mensagemInicial = document.createElement("li");
         mensagemInicial.innerText = `Nenhum aluno cadastro`;
+        mensagemInicial.id = "mensagem-vazia";
         lista.appendChild(mensagemInicial);
         return;
     }
@@ -53,51 +54,76 @@ function renderAlunos() {
 
 function adicionarAlunoNaLista(aluno, index){
 
-        // criar lista de alunos
-        let li = document.createElement("li");
-        li.textContent = `${aluno.nome} - ${aluno.idade} anos`;
-        li.style.marginTop = "8px";
-        lista.appendChild(li);
+    const msg = document.getElementById("mensagem-vazia");
+    if(msg) msg.remove();
 
-        // criar botão remover
-        let removeLi = document.createElement("button");
-        removeLi.innerHTML = "Remover";
-        removeLi.style.marginLeft = "7px";
-        li.appendChild(removeLi);
+    // criar lista de alunos
+    let li = document.createElement("li");
+    li.textContent = `${aluno.nome} - ${aluno.idade} anos`;
+    li.style.marginTop = "8px";
+    li.dataset.id = aluno.id; // Inserir o id do aluno no DOM
+    lista.appendChild(li);
 
-        // animação
-        li.style.maxHeight = '0px'; // Valor inicial
-        li.style.overflow = 'hidden';
-        li.style.transition = 'max-height 1s ease';
+    // criar botão remover
+    let removeLi = document.createElement("button");
+    removeLi.innerHTML = "Remover";
+    removeLi.style.marginLeft = "7px";
+    li.appendChild(removeLi);
 
-        requestAnimationFrame(() => {
-            li.style.maxHeight = li.scrollHeight + 'px'
-        }); // Crescer até o tamanho real do elemento
+    // animação
+    li.style.maxHeight = '0px'; // Valor inicial
+    li.style.overflow = 'hidden';
+    li.style.transition = 'max-height 1s ease';
 
-        // remover aluno correspondente
-        removeLi.addEventListener("click", () => {
-        li.style.maxHeight = li.scrollHeight + 'px'; // Valor inicial
-        /* li.style.overflow = 'hidden';
-        li.style.transition = 'max-height 0.3s ease'; */
+    requestAnimationFrame(() => {
+        li.style.maxHeight = li.scrollHeight + 'px'
+    }); // Crescer até o tamanho real do elemento
 
-        requestAnimationFrame(() => (li.style.maxHeight = '0px'));
+    // remover aluno correspondente (agora buscando por id atual)
+    removeLi.addEventListener("click", () => {
+    li.style.maxHeight = li.scrollHeight + 'px'; // Valor inicial
+    /* li.style.overflow = 'hidden';
+    li.style.transition = 'max-height 0.3s ease'; */
 
-        // remover o item e renderizar só depois da animação
-        li.addEventListener('transitionend', () => {
-            alunos.splice(index,1); // remove do array
-        }, { once:true});
-        }); 
+    requestAnimationFrame(() => (li.style.maxHeight = '0px'));
+
+    // remover o item e renderizar só depois da animação
+    li.addEventListener('transitionend', () => {
+        const id = li.dataset.id;
+        const idx = alunos.findIndex(a => a.id === id);
+        if (idx !== -1){
+        alunos.splice(idx, 1); // remove do array
+        localStorage.setItem("alunos", JSON.stringify(alunos)); // atualiza o localStorage
+        }
+
+        somaEMedia()
+
+        if(!alunos.length) renderAlunos();
+
+    }, { once:true});
+    }); 
 }
 
 function somaEMedia(){
+    // Remover lista antiga
+
+    const antigo = lista.querySelectorAll(".info-extra");
+    antigo.forEach(el => el.remove());
+
+    if(!alunos.length) return;
+    
     // soma das idades para tirar média
     let soma = alunos.reduce((acc, aluno) => acc + aluno.idade, 0);
     let media = soma / alunos.length
     let mediaDeIdades = document.createElement("div");
+    mediaDeIdades.classList.add("info-extra");
     mediaDeIdades.textContent = `Media de idades: ${media}`
 
     // contagem de itens na lista
     let contadorDeItens = document.createElement("div");
+    contadorDeItens.classList.add("info-extra");
+    contadorDeItens.style.marginTop = "15px";
+
     contadorDeItens.textContent = `Total de alunos: ${alunos.length}`;
 
     // exibir na lista se o array não estiver vazio
@@ -113,4 +139,3 @@ deletarTudo.addEventListener("click", () => {
     localStorage.setItem("alunos", JSON.stringify(alunos));
     renderAlunos();
 })
-
